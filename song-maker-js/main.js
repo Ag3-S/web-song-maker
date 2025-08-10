e4859b5985391cf8643a86aaf3d2204486776ea4
Ag3-S <agrima.uiet@gmail.com>
2025-08-10 09:22:46 +0530

    Session 3

--- song-maker-js/main.js
+++ song-maker-js/main.js
@@ -1,9 +1,21 @@
-console.log("Start loading js");
-
 const grid = document.getElementById("grid");
-const myTempH1 = document.createElement("h1");
 
-console.log("Created my temp H1 loading js");
-myTempH1.textContent = "bye bye";
-grid.appendChild(myTempH1);
-console.log("Finish loading js");
+const gridlayout = document.createElement("div");
+gridlayout.className = "grid-layout";
+gridlayout.id = "grid-layout-1"
+
+const labelsLeftSide = document.createElement("div");
+labelsLeftSide.className = "labels-left-side";
+
+const rulerSpacer = document.createElement("div");
+rulerSpacer.className = "ruler-spacer";
+
+
+const rowLabel = document.createElement("div");
+rowLabel.className = "row-label";
+
+labelsLeftSide.appendChild(rulerSpacer);
+labelsLeftSide.appendChild(rowLabel);
+
+gridlayout.appendChild(labelsLeftSide);
+grid.appendChild(gridlayout);
